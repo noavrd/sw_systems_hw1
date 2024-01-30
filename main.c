8890609d03f8e9e58c2d1a58ba843954565fba8e
linoyKaufman <157068348+linoyKaufman@users.noreply.github.com>
2024-01-30 14:31:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,84 @@
 #include <stdio.h>
 #include "NumClass.h"
 
-int isPalindrome(int num);
-
 int main()
 {
-    printf("%d\n", isPalindrome(18));
-    return 0;
+int start;
+int end;
+in length = end - start +1;
+int prime [lengnth];
+int strong [lengnth];
+int armstrong [lengnth];
+int palindrom [lengnth];
+int primeINDX = 0;
+int strongINDX = 0;
+int armstrongINDX = 0;
+int palindromINDX = 0;
+
+
+printf("enter a number:")
+scanf("%d", &start);
+
+printf("enter another number:")
+scanf("%d", &end);
+
+
+
+for (int i = start ; i<=end ; i++)
+{
+    if (isPrime(i)==1)
+    {
+        prime [primeINDX]= i;
+        primeINDX++
+    }
+
+    if (isArmstrong(i)==1)
+    {
+        armstrong[armstrongINDX]=i;
+        armstrongINDX++;
+    }
+
+    if (isStrong(i)==1)
+    {
+        strong[strongINDX]=i;
+        strongINDX++;
+    }
+
+    if (isPalindrome(i)==1)
+    {
+        palindrom[palindromINDX]= i;
+        palindromINDX++;
+    }
+}
+
+printf("the prime numbers are :")
+for (int i = 0; i < primeINDX; i++)
+{
+    printf("%d",prime[i] )
+}
+
+
+printf("\n the armstrong numbers are :")
+for (int i = 0; i < armstrongINDX; i++)
+{
+    printf("%d",armstrong[i] )
+}
+
+
+printf("\n the strong numbers are :")
+for (int i = 0; i < strongINDX; i++)
+{
+    printf("%d",strong[i] )
+}
+
+
+printf("\n the palindrom numbers are :")
+for (int i = 0; i < palindromINDX; i++)
+{
+    printf("%d", palindrom[i] )
+}
+
+
+
+  return 0;
 }

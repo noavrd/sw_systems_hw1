eecfe098867391dce2417d641f6fa9c9fd0fdeef
linoyKaufman <157068348+linoyKaufman@users.noreply.github.com>
2024-01-30 11:55:48 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -29,7 +29,6 @@ int armstrongRecursive(int num, int totalNumbers) {
 
 int isArmstrong(int num) {
     int total = 0;
-    
     int numSum = num;
     int totalNumbers = 0;
 
@@ -46,39 +45,36 @@ int isArmstrong(int num) {
     return 0;
 }
 
-// int reverseNumRecursive(int num, int totalNumbers) {
-//     if (num == 0) {
-//         return 0;
-//     }
+int reverseNum (int num)  // פונקציית עזר לפלינדרום שמחזירה את המספר הפוך
+{
+    int reverse = 0;
+    while(num!=0)
+    {
+        int digit = num%10;
+        reverse = reverse *10 + digit;
+        num = num/10;
+    }
+    return reverse;
 
-//     return reverseNumRecursive()
-// }
+}
+
+int isPalindromehelper(int num,int temp)  //פונקציית עזר לפולינדרום 
+{
+                             
+    if(num == temp)   // אם המספר ההפוך שווה למספר הרגיל
+    {
+        return 1;
+    }
+    if (num%10 != temp%10)
+    {
+       return 0;
+    }
+    else return isPalindromehelper (num/10, temp/10);
+}
 
 int isPalindrome(int num) {
 
-    int originNum = num;
-    int totalNumbers = 0;
-    int palindromNum = 0;
-
-    totalNumbers = countNumbersRecursive(num);
-
-    num = originNum;
-
-    while (num > 0) {
-        int findFirst = 1;
-        for (int i = 1; i < totalNumbers; i++) {
-            findFirst *= 10;
-        }
-
-        findFirst *= (num % 10);
-        palindromNum += findFirst; 
-        totalNumbers--;
-        num /= 10;
-    }
-
-    if (palindromNum == originNum) {
-        return 1;
-    }
-
-    return 0;
+    int temp = reverseNum(num); 
+    return isPalindromehelper (num,temp);
+    
 }

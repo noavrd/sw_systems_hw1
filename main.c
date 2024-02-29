f4ae12e80f20376d4ad4b35a4bf888cc9fbb2d4a
linoyKaufman <157068348+linoyKaufman@users.noreply.github.com>
2024-02-29 16:51:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,82 +3,74 @@
 
 int main()
 {
-int start;
-int end;
-in length = end - start +1;
-int prime [lengnth];
-int strong [lengnth];
-int armstrong [lengnth];
-int palindrom [lengnth];
-int primeINDX = 0;
-int strongINDX = 0;
-int armstrongINDX = 0;
-int palindromINDX = 0;
+    int start = 0;
+    int end = 0;
+    int length = end - start + 1;
+    int prime[length];
+    int strong[length];
+    int armstrong[length];
+    int palindrom[length];
+    int primeINDX = 0;
+    int strongINDX = 0;
+    int armstrongINDX = 0;
+    int palindromINDX = 0;
 
+    printf("enter a number:");
+    scanf("%d", &start);
 
-printf("enter a number:")
-scanf("%d", &start);
+    printf("enter another number:");
+    scanf("%d", &end);
 
-printf("enter another number:")
-scanf("%d", &end);
-
-
-
-for (int i = start ; i<=end ; i++)
-{
-    if (isPrime(i)==1)
+    for (int i = start; i <= end; i++)
     {
-        prime [primeINDX]= i;
-        primeINDX++
+        if (isPrime(i) == 1)
+        {
+            prime[primeINDX] = i;
+            primeINDX++;
+        }
+
+        if (isArmstrong(i) == 1)
+        {
+            armstrong[armstrongINDX] = i;
+            armstrongINDX++;
+        }
+
+        if (isStrong(i) == 1)
+        {
+            strong[strongINDX] = i;
+            strongINDX++;
+        }
+
+        if (isPalindrome(i) == 1)
+        {
+            palindrom[palindromINDX] = i;
+            palindromINDX++;
+        }
     }
 
-    if (isArmstrong(i)==1)
+    printf("the prime numbers are :");
+    for (int i = 0; i < primeINDX; i++)
     {
-        armstrong[armstrongINDX]=i;
-        armstrongINDX++;
+        printf("%d", prime[i]);
     }
 
-    if (isStrong(i)==1)
+    printf("\n the armstrong numbers are :");
+    for (int i = 0; i < armstrongINDX; i++)
     {
-        strong[strongINDX]=i;
-        strongINDX++;
+        printf("%d", armstrong[i]);
     }
 
-    if (isPalindrome(i)==1)
+    printf("\n the strong numbers are :");
+    for (int i = 0; i < strongINDX; i++)
     {
-        palindrom[palindromINDX]= i;
-        palindromINDX++;
+        printf("%d", strong[i]);
     }
-}
 
-printf("the prime numbers are :")
-for (int i = 0; i < primeINDX; i++)
-{
-    printf("%d",prime[i] )
-}
+    printf("\n the palindrom numbers are :");
+    for (int i = 0; i < palindromINDX; i++)
+    {
+        printf("%d", palindrom[i]);
+    }
 
-
-printf("\n the armstrong numbers are :")
-for (int i = 0; i < armstrongINDX; i++)
-{
-    printf("%d",armstrong[i] )
-}
-
-
-printf("\n the strong numbers are :")
-for (int i = 0; i < strongINDX; i++)
-{
-    printf("%d",strong[i] )
-}
-
-
-printf("\n the palindrom numbers are :")
-for (int i = 0; i < palindromINDX; i++)
-{
-    printf("%d", palindrom[i] )
-}
-
-
-
-  return 0;
+    return 0;
 }
